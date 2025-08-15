7d28f5bb35986be82495db0a06dc52aaa360e7aa
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-08-15 15:51:11 -0400

    Enhance consult

--- lisp/modules/mod-consult.el
+++ lisp/modules/mod-consult.el
@@ -39,7 +39,6 @@
              consult-goto-line
              consult-grep
              consult-history
-             consult-info
              consult-isearch-history
              consult-keep-lines
              consult-kmacro
@@ -60,11 +59,64 @@
              consult-register-format
              consult-theme
              consult-yank-pop
-             ;; Extensions
-             consult-imenu
-             consult-xref
-             consult-info)
-  :functions consult--customize-put
+             consult-imenu)
+  :functions (consult--customize-put
+              consult-narrow-help
+              consult-xref)
+
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
 
   :hook
   ;; Enable automatic preview at point in the *Completions* buffer.
@@ -83,50 +135,30 @@
   ;; and hides the mode line of the window.
   (advice-add #'register-preview :override #'consult-register-window)
 
-  ;; Consult xref
   (setq xref-show-xrefs-function #'consult-xref)
   (setq xref-show-definitions-function #'consult-xref)
 
   :config
-  (use-package consult-imenu
-    :ensure nil
-    :commands (consult-imenu
-               consult-imenu-multi))
+  (require 'consult-imenu)
 
-  (use-package consult-xref
-    :ensure nil)
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
 
-  (use-package consult-info
-    :ensure nil
-    :commands consult-info
-    :init
-    (defun consult-info-emacs ()
-      "Search through Emacs info pages."
-      (interactive)
-      (consult-info "emacs" "efaq" "elisp" "cl"))
+  ;; Configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  ;; "C-+"
+  ;; (setq consult-narrow-key "<")
 
-    (defun consult-info-org ()
-      "Search through the Org info page."
-      (interactive)
-      (consult-info "org"))
-
-    (defun consult-info-completion ()
-      "Search through completion info pages."
-      (interactive)
-      (consult-info "vertico" "consult" "marginalia" "orderless" "embark"
-                    "corfu" "cape" "tempel")))
-
-  (consult-customize consult-ripgrep
-                     ;; consult-xref
-                     consult-fd :preview-key '(:debounce 0.1 any)
-                     consult-recent-file
-                     consult--source-file-register
-                     consult--source-recent-file
-                     consult--source-project-recent-file
-                     consult-git-grep consult-grep consult-bookmark
-                     consult-theme :preview-key '(:debounce 0.1 any)
-                     consult--source-bookmark
-                     :preview-key '(:debounce 0.1 any)))
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+  )
 
 ;;; Completing indicator
 
