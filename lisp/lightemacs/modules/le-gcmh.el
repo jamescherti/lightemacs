33d7cfd5fb3a80077dbcf551e62c6b6c899ac293
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-02-10 08:09:10 -0500

    Update le-gcmh

--- lisp/lightemacs/modules/le-gcmh.el
+++ lisp/lightemacs/modules/le-gcmh.el
@@ -32,7 +32,7 @@
   (setq gcmh-verbose init-file-debug
         gcmh-auto-idle-delay-factor 10
 
-        gcmh-high-cons-threshold (* 256 1024 1024)
+        gcmh-high-cons-threshold (* 96 1024 1024)
         gcmh-low-cons-threshold minimal-emacs-gc-cons-threshold
 
         ;; This variable determines how long Emacs should wait (in seconds) while
