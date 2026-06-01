26bb22349421e71164c949407ac4c677e37dd754
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-05-31 21:33:04 -0400

    le-consult: Use lightemacs-module-bind

--- lisp/lightemacs/modules/le-consult.el
+++ lisp/lightemacs/modules/le-consult.el
@@ -25,6 +25,7 @@
 
 (eval-and-compile
   (require 'lightemacs-use-package))
+(require 'lightemacs-module)
 
 ;; Load `lightemacs--ripgrep-executable' and `lightemacs--fdfind-executable'
 (require 'le-core-cli-tools)
@@ -72,62 +73,6 @@
              consult-xref)
   :functions (consult--customize-put
               consult-narrow-help)
-
-  :bind (;; C-c bindings in `mode-specific-map'
-         ("C-c r" . consult-recent-file)
-         ("M-s p" . consult-fd)
-
-         ("C-c M-x" . consult-mode-command)
-         ("C-c h" . consult-history)
-         ;; ("C-c k" . consult-kmacro)
-         ("C-c m" . consult-man)
-         ("C-c i" . consult-info)
-         ([remap Info-search] . consult-info)
-         ;; C-x bindings in `ctl-x-map'
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-         ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-         ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-         ;; M-g bindings in `goto-map'
-         ;; ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings in `search-map'
-         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
-         ("M-s c" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-         ;; Minibuffer history
-         :map minibuffer-local-map
-         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
 
   :preface
   (defun lightemacs-consult--crm-indicator (args)
@@ -196,11 +141,8 @@ them. Ensures this runs only when `crm` is loaded and Consult is in use."
         consult-async-input-throttle 0.2
         consult-async-input-debounce 0.1)
 
-  (setq consult--gc-threshold (* 64 1024 1024))
+  (setq consult--gc-threshold (* 128 1024 1024))
   (setq consult--process-chunk (* 2 1024 1024))
-
-  ;; (setq consult--gc-threshold (* 2 64 1024 1024))
-  ;; (setq consult--process-chunk (* 2 1024 1024))
 
   ;; Fix `elpaca' with `with-eval-after-load'
   (with-eval-after-load 'le-core-cli-tools
@@ -237,6 +179,72 @@ them. Ensures this runs only when `crm` is loaded and Consult is in use."
   ;; You may want to use `embark-prefix-help-command' or which-key instead.
   ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
   )
+
+;;; Keybindings
+
+(lightemacs-module-bind consult
+  ;; C-c bindings in `mode-specific-map'
+  (keymap-global-set "C-c r" #'consult-recent-file)
+  (keymap-global-set "M-s p" #'consult-fd)
+
+  (keymap-global-set "C-c M-x" #'consult-mode-command)
+  (keymap-global-set "C-c h" #'consult-history)
+  ;; (keymap-global-set "C-c k" #'consult-kmacro)
+  (keymap-global-set "C-c m" #'consult-man)
+  (keymap-global-set "C-c i" #'consult-info)
+  (keymap-global-set "<remap> <Info-search>" #'consult-info)
+
+  ;; C-x bindings in `ctl-x-map'
+  (keymap-global-set "C-x M-:" #'consult-complex-command)
+  (keymap-global-set "C-x b" #'consult-buffer)
+  (keymap-global-set "C-x 4 b" #'consult-buffer-other-window)
+  (keymap-global-set "C-x 5 b" #'consult-buffer-other-frame)
+  (keymap-global-set "C-x t b" #'consult-buffer-other-tab)
+  (keymap-global-set "C-x r b" #'consult-bookmark)
+  (keymap-global-set "C-x p b" #'consult-project-buffer)
+
+  ;; Custom M-# bindings for fast register access
+  (keymap-global-set "M-#" #'consult-register-load)
+  (keymap-global-set "M-'" #'consult-register-store)
+  (keymap-global-set "C-M-#" #'consult-register)
+
+  ;; Other custom bindings
+  (keymap-global-set "M-y" #'consult-yank-pop)
+
+  ;; M-g bindings in `goto-map'
+  ;; (keymap-global-set "M-g e" #'consult-compile-error)
+  (keymap-global-set "M-g f" #'consult-flymake)
+  (keymap-global-set "M-g g" #'consult-goto-line)
+  (keymap-global-set "M-g M-g" #'consult-goto-line)
+  (keymap-global-set "M-g o" #'consult-outline)
+  (keymap-global-set "M-g m" #'consult-mark)
+  (keymap-global-set "M-g k" #'consult-global-mark)
+  (keymap-global-set "M-g i" #'consult-imenu)
+  (keymap-global-set "M-g I" #'consult-imenu-multi)
+
+  ;; M-s bindings in `search-map'
+  (keymap-global-set "M-s d" #'consult-find)
+  (keymap-global-set "M-s c" #'consult-locate)
+  (keymap-global-set "M-s g" #'consult-grep)
+  (keymap-global-set "M-s G" #'consult-git-grep)
+  (keymap-global-set "M-s r" #'consult-ripgrep)
+  (keymap-global-set "M-s l" #'consult-line)
+  (keymap-global-set "M-s L" #'consult-line-multi)
+  (keymap-global-set "M-s k" #'consult-keep-lines)
+  (keymap-global-set "M-s u" #'consult-focus-lines)
+
+  ;; Isearch integration
+  (keymap-global-set "M-s e" #'consult-isearch-history)
+
+  (with-eval-after-load 'isearch
+    (keymap-set isearch-mode-map "M-e" #'consult-isearch-history)
+    (keymap-set isearch-mode-map "M-s e" #'consult-isearch-history)
+    (keymap-set isearch-mode-map "M-s l" #'consult-line)
+    (keymap-set isearch-mode-map "M-s L" #'consult-line-multi))
+
+  ;; Minibuffer history
+  (keymap-set minibuffer-local-map "M-s" #'consult-history)
+  (keymap-set minibuffer-local-map "M-r" #'consult-history))
 
 ;;; Provide
 
