85f60d97533a9d8687461e9a93b65d6a6b1dae54
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-08-21 11:15:36 -0400

    Refactor le-dired-filter to manage stack and persist minor mode
    - Ensure dired-filter-mode remains active in all Dired buffers to retain
      keybindings and extra features.
    - Update lightemacs-dired-filter--apply-state to strictly add or remove
      the filters defined in lightemacs-dired-filter-setup-hook from the
      dired-filter-stack.
    - Ensure the buffer refreshes correctly when toggling filters by
      tracking stack modifications and calling dired-filter-update.

--- lisp/lightemacs/modules/le-dired-filter.el
+++ lisp/lightemacs/modules/le-dired-filter.el
@@ -28,7 +28,6 @@
 ;; `dired-filter-by-omit' is configured by le-dired. It not only hides
 (defvar lightemacs-dired-filter-setup-hook '(dired-filter-by-omit)
   "Hook of Dired filter functions to apply when entering `dired-mode'.
-
 For instance:
   ;; Hide files ignored by Git (may be slow in directories with many files)
   (add-hook \\='lightemacs-dired-filter-setup-hook
@@ -43,7 +42,8 @@ For instance:
 ;;; Use-package dired-filter
 
 (lightemacs-use-package dired-filter
-  :commands (dired-filter-pop-all
+  :commands (dired-filter-mode
+             dired-filter-pop-all
              dired-filter-by-git-ignored
              dired-filter-by-dot-files
              dired-filter-by-omit)
@@ -55,40 +55,47 @@ For instance:
         lightemacs-dired-filter-global-enabled
       lightemacs-dired-filter-local-enabled))
 
-  (defun lightemacs-dired-filter--enable-filters ()
-    "Enable `dired' filters based on user configuration."
-    (when (derived-mode-p 'dired-mode)
-      (let ((enabled (lightemacs-dired-filter--enabled))
-            (changed nil))
-        (dolist (filter-func lightemacs-dired-filter-setup-hook)
-          (when (fboundp filter-func)
-            (let* ((filter-name-str (replace-regexp-in-string
-                                     "^dired-filter-by-" ""
-                                     (symbol-name filter-func)))
-                   (filter-name (intern filter-name-str))
-                   (is-active (and (boundp 'dired-filter-stack)
-                                   (assq filter-name dired-filter-stack))))
-              (if enabled
-                  (unless is-active
-                    (funcall filter-func)) ; triggers autoload and add filter
-                (when is-active
-                  (setq dired-filter-stack
-                        (assq-delete-all filter-name dired-filter-stack))
-                  (setq changed t))))))
-        (when changed
-          (if (fboundp 'dired-filter-update)
-              (dired-filter-update)
-            (revert-buffer))))))
-
   (defun lightemacs-dired-filter--apply-state ()
-    "Save point, apply the filter state, and restore point."
-    (when (and (fboundp 'dired-goto-file)
+    "Apply the Dired filter state and restore point.
+This function enables `dired-filter-mode' and either applies or removes
+the filters defined in `lightemacs-dired-filter-setup-hook' based on
+the current state."
+    (when (and (derived-mode-p 'dired-mode)
                (fboundp 'dired-get-file-for-visit))
       (let ((dired-file (condition-case nil
                             (dired-get-file-for-visit)
                           (error nil))))
-        (lightemacs-dired-filter--enable-filters)
-        (when dired-file
+        ;; Enable filters
+        (let ((enabled (lightemacs-dired-filter--enabled))
+              (changed nil))
+          (unless (bound-and-true-p dired-filter-mode)
+            (dired-filter-mode 1))
+
+          (dolist (filter-func lightemacs-dired-filter-setup-hook)
+            (when (fboundp filter-func)
+              (let* ((filter-name-str (replace-regexp-in-string
+                                       "^dired-filter-by-" ""
+                                       (symbol-name filter-func)))
+                     (filter-name (intern filter-name-str))
+                     (is-active (and (boundp 'dired-filter-stack)
+                                     (assq filter-name dired-filter-stack))))
+                (if enabled
+                    (unless is-active
+                      (funcall filter-func) ; triggers autoload and add filter
+                      (setq changed t))
+                  (when is-active
+                    (setq dired-filter-stack
+                          (assq-delete-all filter-name dired-filter-stack))
+                    (setq changed t))))))
+
+          (when changed
+            (if (fboundp 'dired-filter-update)
+                (dired-filter-update)
+              (revert-buffer))))
+
+        ;; Go to file
+        (when (and dired-file
+                   (fboundp 'dired-goto-file))
           (dired-goto-file dired-file)))))
 
   (defun lightemacs-dired-filter-local-toggle ()
@@ -126,7 +133,7 @@ local value was set by the user."
       (define-key dired-mode-map (kbd "C-c f") #'lightemacs-dired-filter-local-toggle)
       (define-key dired-mode-map (kbd "C-c F") #'lightemacs-dired-filter-global-toggle)))
 
-  (add-hook 'dired-mode-hook #'lightemacs-dired-filter--apply-state))
+  (add-hook 'dired-mode-hook #'lightemacs-dired-filter--apply-state 80))
 
 (provide 'le-dired-filter)
 
