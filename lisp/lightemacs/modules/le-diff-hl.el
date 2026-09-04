0d5f0a2344b97389ea25d293635a3a07d6d53b0b
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-03 21:58:55 -0400

    le-diff-hl: Update diff-hl after switching to a buffer

--- lisp/lightemacs/modules/le-diff-hl.el
+++ lisp/lightemacs/modules/le-diff-hl.el
@@ -20,20 +20,63 @@
 
 ;;; Code:
 
+;;; Require
+
 (require 'lightemacs-module)
 (eval-and-compile
   (require 'lightemacs-use-package))
 
+;;; use-package diff-hl
+
 (lightemacs-use-package diff-hl
   :commands (diff-hl-mode
              global-diff-hl-mode
+             diff-hl-flydiff
              diff-hl-update)
   :init
+  (lightemacs-module-hooks diff-hl-global
+    global-diff-hl-mode
+    '(lightemacs-on-first-file-hook))
+
+  (lightemacs-module-hooks diff-hl-local
+    diff-hl-mode
+    nil)
+
+  (lightemacs-module-hooks diff-hl-flydiff-global
+    diff-hl-flydiff-mode
+    '(global-diff-hl-mode-hook))
+
   (lightemacs-module-setq-maybe diff-hl
     diff-hl-flydiff-delay 0.4  ; Faster
+    diff-hl-draw-borders nil
+    diff-hl-disable-on-remote t
     diff-hl-show-staged-changes nil  ; Realtime feedback
     diff-hl-update-async t  ; Do not block Emacs
     diff-hl-global-modes '(not pdf-view-mode image-mode)))
+
+
+;;; Bug fix: Update diff-hl after switching to a buffer
+
+;; TODO: Send a patch to diff-hl?
+
+(defun lightemacs--diff-hl-update-hook (window)
+  "Update diff-hl when WINDOW buffer changes, if `diff-hl-mode' is enabled."
+  (let ((buffer (window-buffer window)))
+    (when (buffer-live-p buffer)
+      (with-current-buffer buffer
+        (when (and (bound-and-true-p diff-hl-mode)
+                   (fboundp 'diff-hl-update))
+          (diff-hl-update))))))
+
+(defun lightemacs--diff-hl-update-on-buffer-change ()
+  "Add or remove the diff-hl update hook based on the state of `diff-hl-mode'."
+  (if diff-hl-mode
+      (add-hook 'window-buffer-change-functions #'lightemacs--diff-hl-update-hook nil t)
+    (remove-hook 'window-buffer-change-functions #'lightemacs--diff-hl-update-hook t)))
+
+(add-hook 'diff-hl-mode-hook #'lightemacs--diff-hl-update-on-buffer-change)
+
+;;; Provide
 
 (provide 'le-diff-hl)
 
