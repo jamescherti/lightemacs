ce2ff32b907b0e6edf864c8a407843309f55e0b2
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-04 14:06:37 -0400

    Update le-corfu

--- lisp/lightemacs/modules/le-corfu.el
+++ lisp/lightemacs/modules/le-corfu.el
@@ -25,7 +25,8 @@
 
 (lightemacs-use-package corfu
   :commands (global-corfu-mode
-             corfu-mode)
+             corfu-mode
+             corfu-history-mode)
 
   :init
   (lightemacs-module-hooks corfu-global
@@ -46,7 +47,7 @@
                               term-mode
                               vterm-mode)
                          t)
-    corfu-auto nil ; Security
+    corfu-auto nil ; Security and performance default
     corfu-auto-delay 0.24
     corfu-auto-prefix 2
     corfu-count 15
@@ -61,15 +62,27 @@
     ;; boundary defined by the active completion-at-point-function (Capf).
     ;; You can freely type spaces or delimiter characters to supply multiple
     ;; filtering terms to Orderless without the session terminating.
-    corfu-quit-at-boundary nil
+    corfu-quit-at-boundary (if corfu-auto 'separator nil)
 
     ;; Prevent the popup from closing when a typo results in zero matches.
     ;; This allows using Backspace to correct the input and restore candidates.
-    corfu-quit-no-match nil)
+    corfu-quit-no-match (if corfu-auto 'separator nil))
 
   (when lightemacs-corfu-add-to-savehist
     (with-eval-after-load 'savehist
-      (add-to-list 'savehist-additional-variables 'corfu-history))))
+      (add-to-list 'savehist-additional-variables 'corfu-history)))
+
+  ;; Corfu: Corfu candidate lists are typically shorter, so its default sorting
+  ;; is simpler. To get it to sort by recency, you must explicitly enable
+  ;; corfu-history-mode.
+  ;;
+  ;; Comparison with Corfu Prescient: While corfu-history sorts purely by
+  ;; recency (your most recent selection goes to the top), Prescient tracks both
+  ;; recency and frequency. With corfu-history, selecting a rare candidate once
+  ;; will immediately place it above your most frequently used candidates.
+  ;; Prescient prevents this by keeping high-frequency candidates near the top
+  ;; even if they were not the absolute last item selected.
+  (lightemacs-module-hooks corfu-history corfu-history-mode '()))
 
 (provide 'le-corfu)
 
