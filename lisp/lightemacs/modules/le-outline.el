ecdcbabbfeb4b041da15d8188c0dbd812492b76e
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-09-18 22:14:26 -0400

    Add hooks to outline minor mode

--- lisp/lightemacs/modules/le-outline.el
+++ lisp/lightemacs/modules/le-outline.el
@@ -24,7 +24,12 @@
 
 (lightemacs-module-hooks outline-minor
   outline-minor-mode
-  '())
+  '(emacs-lisp-mode-hook
+    lisp-interaction-mode-hook ; scratch
+    lisp-mode-hook
+    conf-mode-hook
+    markdown-mode-hook
+    diff-mode-hook))
 
 ;;; Update the outline ellipsis
 
